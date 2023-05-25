754f7b3ab56f78e81c4c5b0bf02e5c9d907e349a
DtechB <d.bayati28@gmail.com>
2023-05-25 19:56:27 +0330

    feat: add fonts and config base colors

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div>خوش آمدید</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      transparent: "transparent",
+      text: "#515553",
+      primary: "#6fc54c",
+      secondary: "#325343",
+      button: "#9bd58b",
+      yellow: "#f8e16c",
+      red: "#fc5555",
+    },
   },
   plugins: [],
 };
